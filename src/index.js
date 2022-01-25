71e7bd4d437cf847300cab0028fb24d51495a875
getfull <gtfullz@gmail.com>
2022-01-25 15:40:57 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    return matrix.reduce(
+        (result, item, index) =>
+            index % 2 ? result.concat(item.reverse()) : result.concat(item),
+        []
+    );
+};
